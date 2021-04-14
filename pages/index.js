7f2654cadc57c64078b3ea333c9ff3c96da6ff50
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-14 13:41:29 -0400

    brief introduction into capabilities of NextJS, the difference between SSR and Client Side Rendering, explains styles and public folder, and how the pages folder routes us through the different pages of the application

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev Next Course</title>
+        <meta name="keywords" content="development, programming" />
+      </Head>
+
+      <h1>Welcome to Next</h1>
+    </div>
+  );
+}
