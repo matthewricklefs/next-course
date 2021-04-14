18ce4ddf99790561dcc5d1cf6619bda9fa328cd8
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-14 14:03:23 -0400

    brief overview of the capabilities of the Document component to augment the html of our application

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,15 @@
 import headerStyles from "../styles/Header.module.css";
 
 const Header = () => {
-  const x = 2;
-
   return (
     <div>
-      <h1 className="title">
+      <h1 className={headerStyles.title}>
         <span>WebDev</span> News
       </h1>
-      <style jsx>
-        {`
-          .title {
-            color: ${x > 3 ? "red" : "blue"};
-          }
-        `}
-      </style>
+
+      <p className={headerStyles.description}>
+        Keep up to date with the latest Web Development news
+      </p>
     </div>
   );
 };
